a6f10593ab7cbd444cebe07d2260628935ad7d43
Senesael Robbe <robbe.senesael@student.howest.be>
2018-05-14 10:15:55 +0200

    added the project

---
+++ sw.js
@@ -0,0 +1,18 @@
+"use strict";
+
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open("v1").then(cache => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/assets/",
+                "/assets/reset.css",
+                "/assets/screen.css",
+                "/js/",
+                "/js/script.js",
+                "/images/"
+            ])
+        })
+    )
+});
